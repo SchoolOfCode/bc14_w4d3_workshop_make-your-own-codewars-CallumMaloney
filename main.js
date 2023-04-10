a15cf6dc3b672217aa0f76b31526ae16e8c6be55
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-04-10 19:40:39 +0100

    Imposters - Kata And Tests Complete!

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 /* 
-👉 Write your kata here!
+Imposters! -
+
+There are imposters among the group...Find them and remove them!!
+
+Given a string of characters, get rid of all the imposters! All imposters are any characters that AREN'T lower case alphabetical characters OR characters that are 
+within parentheses!! So any characters between parentheses should NOT be removed. The parentheses should be removed to let the characters within rejoin the group!
+
+EXAMPLE - 
+                      OUTPUT
+"HelloMyNam(3!)1s"   ---------> "elloyam3!s" 
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+
+
+export function removeImposters(str) {
+   
+    return ''
+  }
+ 
--- main.test.js
+++ main.test.js
@@ -1,1 +1,25 @@
 //👉 Write your tests below here:
+
+import {removeImposters} from './main.js'
+import {test, expect} from "@jest/globals"
+
+test("Uppercase",() =>{
+
+    expect(removeImposters('aBcdEfGHiJKL')).toBe('acdfi')
+
+})
+
+test("Numbers", () => {
+
+    expect(removeImposters('one23fourfive67(8)nine')).toBe('onefourfive8nine')
+})
+
+test("Special", () =>{
+
+    expect(removeImposters('hel.lowh/a@tisyo,urn[am]e')).toBe('hellowhatisyourname')
+})
+
+test("Random", () => {
+
+    expect(removeImposters('Aqu(IC)k, br0wn Fox!!')).toBe('quICkbrwnox')
+})
